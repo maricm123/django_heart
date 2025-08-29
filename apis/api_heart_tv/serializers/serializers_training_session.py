7c1ab2f9f19ca6ff8fbe2bb5a562ff4710cd01a1
mmaric123 <mihailomaric001@gmail.com>
2025-08-29 12:46:41 +0200

    changed startand finish sessions serializers

--- apis/api_heart_tv/serializers/serializers_training_session.py
+++ apis/api_heart_tv/serializers/serializers_training_session.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 from gym.models import GymTenant
 from training_session.models import TrainingSession
+from user.models import Coach
 from user.models.client import Client
 
 
@@ -10,7 +11,6 @@ class CreateTrainingSessionSerializer(serializers.Serializer):
     title = serializers.CharField()
     start = serializers.DateTimeField(required=True)
     client_id = serializers.IntegerField(required=True)
-    gym_id = serializers.IntegerField(required=True)
 
     def validate_start(self, value):
         print(value, "Value start")
@@ -21,22 +21,22 @@ class CreateTrainingSessionSerializer(serializers.Serializer):
     def validate_client_id(self, value):
         return get_object_or_404(Client, id=value)
 
-    def validate_gym_id(self, value):
-        return get_object_or_404(GymTenant, id=value)
-
     def validate(self, data):
-        print(data, "DATAAA")
-
         start = data.get('start')
         title = data.get('title')
         client_id = data.get('client_id')
-        gym_id = data.get('gym_id')
+        coach_id = data.get('coach_id')
+
+        user = self.context['request'].user
+        coach = Coach.objects.get(user=user)
 
         session = TrainingSession.start_session(
             start=start,
             title=title,
             client=client_id,
-            gym=gym_id
+            coach=coach,
+            gym=coach.gym,
+            is_active=True
         )
         data['id'] = session.id
         return data
