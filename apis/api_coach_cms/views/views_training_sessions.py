f13cc73738cdb1782fd8036583d86f5527887e27
mmaric123 <mihailomaric001@gmail.com>
2025-09-15 17:58:05 +0200

    changed order of training sessions

--- apis/api_coach_cms/views/views_training_sessions.py
+++ apis/api_coach_cms/views/views_training_sessions.py
@@ -22,4 +22,4 @@ class GetAllTrainingSessionsPerClientView(generics.ListAPIView):
 
     def get_queryset(self):
         client_id = self.kwargs.get('id')
-        return TrainingSession.objects.filter(client__id=client_id, is_active=False)
+        return TrainingSession.objects.filter(client__id=client_id, is_active=False).order_by('-start')
