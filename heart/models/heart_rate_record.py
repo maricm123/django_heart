4e9904f2703f73fe8fe680ba0d5ae4afb997c6d3
mmaric123 <mihailomaric001@gmail.com>
2025-10-24 22:07:15 +0200

    added new fields to heart rate record

--- heart/models/heart_rate_record.py
+++ heart/models/heart_rate_record.py
@@ -6,8 +6,11 @@ from user.models import Client
 class HeartRateRecord(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     device_id = models.CharField(max_length=100)
+    # add later , this reduces storage and makes joins cleaner
+    # device = models.ForeignKey(Device, on_delete=models.SET_NULL, null=True)
     bpm = models.PositiveIntegerField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)  # timestamp from server
+    session_timestamp = models.DateTimeField(blank=True, null=True)  # actual beat time (or equal to timestamp by default)
 
     training_session = models.ForeignKey(
         TrainingSession,
@@ -16,6 +19,12 @@ class HeartRateRecord(models.Model):
         blank=True,
         related_name='heart_rate_records'
     )
+
+    class Meta:
+        #  because aggregation will query training session per timestamp
+        indexes = [
+            models.Index(fields=['training_session', 'timestamp']),
+        ]
 
     def __str__(self):
         return str(self.client) + " " + str(self.device_id)
