faff64c619f7f8ef7f22054a30fe7b9866644f86
mmaric123 <mihailo.maric@s7codedesign.com>
2025-11-24 15:07:23 +0100

    added deleted_at field and view for deleting exercise (soft)

--- training_session/exceptions.py
+++ training_session/exceptions.py
@@ -5,3 +5,9 @@ class TrainingSessionMetricsProcessingError(DomainError):
     default_message = "Failed to process training metrics."
     default_code = "metrics_processing_error"
     default_status_code = 500
+
+
+class CannotDeleteActiveTrainingSessionError(DomainError):
+    default_message = "Cannot delete active training session"
+    default_code = "delete_error"
+    default_status_code = 500
