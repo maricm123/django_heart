4f16db8f6c182973e62d8ee4e5d7b176331892fe
mmaric123 <mihailomaric001@gmail.com>
2025-11-27 16:24:25 +0100

    added some tests for client heart rate

--- training_session/tests/tests.py
+++ training_session/tests/tests.py
@@ -1,8 +1,11 @@
 from training_session.exceptions import CannotDeleteActiveTrainingSessionError
 from training_session.selectors import training_session_per_client_list_data
+from training_session.services import get_client_max_heart_rate
 from training_session.tests.factories import TrainingSessionFactory
 import pytest
 from django.utils import timezone
+from datetime import date
+from user.tests.factories import ClientFactory, UserFactory
 
 
 @pytest.mark.django_db
@@ -63,3 +66,57 @@ class TestTrainingSession:
 
         with pytest.raises(CannotDeleteActiveTrainingSessionError):
             session.delete()
+
+    def test_get_client_max_heart_rate_from_database(self, session_env):
+        tenant = session_env["tenant"]
+        coach = session_env["coach"]
+        user = UserFactory(
+            birth_date=date(2010, 0o7, 18)
+        )
+        client2 = ClientFactory(
+            user=user,
+            coach=coach,
+            gym=tenant,
+            max_heart_rate=200
+        )
+
+        samples = [
+            ("2025-01-10T10:00:00Z", 91),
+            ("2025-01-10T10:01:00Z", 92),
+            ("2025-01-10T10:02:00Z", 95),
+            ("2025-01-10T10:03:00Z", 94),
+            ("2025-01-10T10:04:00Z", 97),
+        ]
+
+        max_hr = get_client_max_heart_rate(client2, samples)
+
+        assert max_hr == client2.max_heart_rate
+        assert max_hr != client2.user.age
+
+    def test_get_client_max_heart_rate_from_session(self, session_env):
+        tenant = session_env["tenant"]
+        coach = session_env["coach"]
+        user = UserFactory(
+            birth_date=date(2010, 0o7, 18)
+        )
+        client2 = ClientFactory(
+            user=user,
+            coach=coach,
+            gym=tenant,
+            max_heart_rate=190
+        )
+
+        samples = [
+            ("2025-01-10T10:00:00Z", 199),
+            ("2025-01-10T10:01:00Z", 92),
+            ("2025-01-10T10:02:00Z", 95),
+            ("2025-01-10T10:03:00Z", 94),
+            ("2025-01-10T10:04:00Z", 97),
+        ]
+
+        max_hr = get_client_max_heart_rate(client2, samples)
+
+        assert max_hr == 199
+        # Make sure it is updated in database
+        assert max_hr == client2.max_heart_rate
+        assert max_hr != client2.user.age
