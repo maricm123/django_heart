906d0b9ce6ca5780aa0dc0857be1d248e6687302
mmaric123 <mihailomaric001@gmail.com>
2025-12-16 19:34:40 +0100

    added skelet for dashbaord view

--- apis/api_coach_cms/views/views_dashboard.py
+++ apis/api_coach_cms/views/views_dashboard.py
@@ -1,6 +1,14 @@
 from rest_framework import generics, status
+from rest_framework import serializers
+from rest_framework.response import Response
 
 
 class DashboardInformationsView(generics.GenericAPIView):
+    class OutputSerializer(serializers.Serializer):
+        gym_name = serializers.CharField(source="gym_tenant.name")
     def get(self, request):
-        pass
+        dashboard_info = get_dashboard_info()
+
+        data = self.OutputSerializer().data
+
+        return Response(data)
